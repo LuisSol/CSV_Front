fa433207d37475238165ac26e27628ef6261276a
Luis Enrique Solis Navarro <luis.solis.ext@geodis.com>
2021-04-17 16:40:59 -0500

    ft(front) - added fileupload styling

---
+++ src/pages/Home/Home.reducer.ts
@@ -0,0 +1,36 @@
+type Action =
+  | {
+      type: "SET_SELECTED_FILE";
+      filename: string;
+      file: File;
+    }
+  | {
+      type: "SET_PROVIDER";
+      provider: string;
+    };
+
+type State = {
+  file: File | null;
+  filename: string;
+  provider: string;
+};
+
+export const homeDefaultState: State = {
+  file: null,
+  filename: "",
+  provider: "",
+};
+
+export const homeReducer = (
+  state: State = homeDefaultState,
+  action: Action
+) => {
+  switch (action.type) {
+    case "SET_SELECTED_FILE":
+      return { ...state, file: action.file, filename: action.filename };
+    case "SET_PROVIDER":
+      return { ...state, provider: action.provider };
+    default:
+      return state;
+  }
+};
--- src/pages/Home/Home.styled.ts
+++ src/pages/Home/Home.styled.ts
@@ -1,13 +1,78 @@
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export const Title = styled.h1``;
 
-export const UploaderWrapper = styled.section``;
+export const UploaderWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-export const FileSelector = styled.input``;
+export const FileSelector = styled.input`
+  display: none;
+`;
 
-export const ProviderTextInput = styled.input``;
+export const InputTextLabel = styled.label`
+  display: block;
+  align-self: flex-start;
+  margin: 20px 0 10px 5px;
+`;
 
-export const UploadBtn = styled.button``;
+export const ProviderTextInput = styled.input`
+  border: 0;
+  border-bottom: 1px solid ${({ theme: { colors } }) => colors.black};
+  width: 300px;
+  margin-bottom: 20px;
+  height: 20px;
+  padding-bottom: 5px;
+  font-size: 19px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const UploadBtn = styled.button`
+  background-color: ${({ theme: { colors } }) => colors.aquaBlue};
+  font-size: 20px;
+  font-weight: 700;
+  color: ${({ theme: { colors } }) => colors.white};
+  display: block;
+  margin-top: 20px;
+  width: 315px;
+  border: 0;
+  height: 40px;
+  border-radius: 5px;
+  cursor: pointer;
+
+  opacity: ${({ disabled }) => (disabled ? ".5" : 1)};
+  pointer-events: ${({ disabled }) => (disabled ? "none" : "all")};
+
+  &:hover {
+    background-color: ${({ theme: { colors } }) => colors.darkAquaBlue};
+  }
+`;
+
+export const FileSelectorLabel = styled.label`
+  display: block;
+  margin-bottom: 20px;
+  cursor: pointer;
+  text-align: center;
+
+  strong {
+    left: 50%;
+    transform: translateX(-50%);
+    position: absolute;
+  }
+`;
+
+export const IconWrapper = styled.div`
+  svg {
+    width: 50px;
+  }
+`;
